58f30a30e549481c943342073fa9a767d1e45845
Scroll Media <chase@getscrollmedia.com>
2026-02-17 09:23:11 -0500

    v3.0: Token security + brand compliance

--- config/client-tokens.js
+++ config/client-tokens.js
@@ -12,7 +12,7 @@
 
 const CLIENT_TOKENS = {
   // Launch Party (Indie Beauty Boutique)
-  '8k3h9x2n': {
+  'r8kh3m2p': {
     clientFolder: 'launchparty',
     clientName: 'Launch Party',
     created: '2026-02-16',
@@ -20,7 +20,7 @@ const CLIENT_TOKENS = {
   },
   
   // Skin by Brownlee (Med Spa)
-  'm4p7w1qz': {
+  't4nw9x5q': {
     clientFolder: 'skinbybrownlee',
     clientName: 'Skin by Brownlee',
     created: '2026-02-16',
@@ -28,7 +28,7 @@ const CLIENT_TOKENS = {
   },
   
   // Define Oakley (Boutique Fitness)
-  'x9n2k5rt': {
+  'v7bj2k6n': {
     clientFolder: 'defineoakley',
     clientName: 'Define Oakley',
     created: '2026-02-16',
